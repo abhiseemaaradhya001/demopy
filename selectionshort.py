6497d9dcbb45c4342924e9f343834180bc83b85c
abhiseemaaradhya001 <121481680+abhiseemaaradhya001@users.noreply.github.com>
2022-12-27 13:13:26 +0530

    Create selectionshort.py

---
+++ selectionshort.py
@@ -0,0 +1,17 @@
+def short(nums):
+
+   for i in range(5):
+       minpos = i
+       for j in range(i,6):
+           if nums[j] < nums[minpos]:
+               minpos= j
+
+       temp = nums[i]
+       nums[i] = nums[minpos]
+       nums[minpos] = temp
+
+       print(nums)
+
+nums = [5,3,8,6,7,2]
+short(nums)
+print(nums)
